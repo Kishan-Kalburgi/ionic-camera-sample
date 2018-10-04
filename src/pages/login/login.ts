dd231d9a3ece3c192c7080367b4bcc00c55de5b9
kishan kalburgi <kishan.ks@hotmail.com>
2018-10-04 01:25:03 -0500

    first commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { User } from '../../providers';
 import { MainPage } from '../';
 
@@ -11,6 +11,7 @@ import { MainPage } from '../';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  public base64Image: string;
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
@@ -21,8 +22,9 @@ export class LoginPage {
 
   // Our translated text strings
   private loginErrorString: string;
-
+  
   constructor(public navCtrl: NavController,
+    private camera: Camera,
     public user: User,
     public toastCtrl: ToastController,
     public translateService: TranslateService) {
@@ -45,6 +47,23 @@ export class LoginPage {
         position: 'top'
       });
       toast.present();
+    });
+  }
+
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
     });
   }
 }
